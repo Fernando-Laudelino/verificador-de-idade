d2d893104406779053f0667edfdacc9370b339cb
fernando laudelino <nandoverananda@gmail.com>
2023-03-23 17:24:48 -0300

    achar o erro nos html e corrigir

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function teste(){
-    
+function verificar(){
+    var date= new Date()
+    var ano = date.getFullYear()
+    var fano = window.document.getElementById('txtano')
+    var res = window.document.getElementById('res')
+
+    if(fano.value.length == 0 || Number(fano.value) > ano){
+        window.alert('Digite o ano correto ')
+    }else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        if (fsex[0].Checked){
+            genero = 'Homem'
+        }else if(fsex[1].Checked){
+            genero = 'Mulher'
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    }
 }
