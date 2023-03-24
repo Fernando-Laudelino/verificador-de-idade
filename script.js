3fcc9b0cc3002853d192e7b6ae1588b036a0b1ab
fernando laudelino <nandoverananda@gmail.com>
2023-03-24 16:22:25 -0300

    adicionei o campo para o nome

--- script.js
+++ script.js
@@ -2,19 +2,23 @@ function verificar(){
     var date= new Date()
     var ano = date.getFullYear()
     var fano = window.document.getElementById('txtano')
+    var nome = document.getElementById('txtnome').value
     var res = window.document.getElementById('res')
 
-    if(fano.value.length == 0 || Number(fano.value) > ano){
+    if(fano.value.length != 4 || Number(fano.value) > ano){
         window.alert('Digite o ano correto ')
     }else{
-        var fsex = document.getElementsByName('radsex')
+    
         var idade = ano - Number(fano.value)
-        var genero = ''
-        if (fsex[0].Checked){
-            genero = 'Homem'
-        }else if(fsex[1].Checked){
-            genero = 'Mulher'
+        var genero = document.querySelector('input[name="radsex"]:checked').value
+        if (genero == 'mas'){
+            genero = 'Masculino'
+        }else {
+            genero = 'Feminino'
         }
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        
+        res.innerHTML = `${nome} detectamos o seu gênero: ${genero} idade: ${idade}`
+         
     }
+
 }
